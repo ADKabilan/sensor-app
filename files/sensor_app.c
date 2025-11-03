ce1dc1095b8502d982e0ac986cc41b3a1f55a134
ADKabilan <kabilan.a@thecflux.com>
2025-11-03 15:23:16 +0530

    added .c & .h

--- files/sensor_app.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() 
-{
-    printf("Sensor layer test success - cflux!\n");
-    printf("done with .bbappend\n");
-    printf("got the source from github repo\n");
-    return 0;
-}
---
+++ files/sensor_lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int readsensor() {
+    printf("reading sensor value...\n");
+    return 18;
+}
